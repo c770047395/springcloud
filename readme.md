737f9e2f4491c1b0dfd519130553cdce4398ddf6
cp <770047395@qq.com>
2020-01-16 19:35:08 +0800

    Eureka生产者配置

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # SpringCloud
 
-## Eureka
+## 1.Eureka
  Eureka是注册中心，提供服务的注册与发现，Eureka服务器需要的依赖如下
  
 ```xml
@@ -39,6 +39,48 @@ eureka:
 public class EurekaServer_7001 {
     public static void main(String[] args) {
         SpringApplication.run(EurekaServer_7001.class,args);
+    }
+}
+```
+
+### 1.1Eureka生产者（服务提供者）
+Eureka生产者同样需要配置依赖
+```xml
+<dependency>
+    <groupId>org.springframework.cloud</groupId>
+    <artifactId>spring-cloud-starter-eureka</artifactId>
+    <version>1.4.7.RELEASE</version>
+</dependency>
+
+<dependency>
+    <groupId>org.springframework.boot</groupId>
+    <artifactId>spring-boot-starter-actuator</artifactId>
+</dependency>
+```
+
+然后配置配置文件
+```yaml
+#Eureka的配置,服务注册到哪里
+eureka:
+  client:
+    service-url:
+     defaultZone: http://localhost:7001/eureka/
+  instance:
+    instance-id: springcloud-provider-dept-8001 #修改Eureka的默认描述信息
+
+info:
+  app.name: cp-springcloud
+  company.name: blog.cp.com
+```
+
+最后加上注解即可使用
+```java
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class DeptProvider_8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptProvider_8001.class,args);
     }
 }
 ```
--- springcloud-provider-dept-8001/src/main/java/com/cp/springcloud/controller/DeptController.java
+++ springcloud-provider-dept-8001/src/main/java/com/cp/springcloud/controller/DeptController.java
@@ -3,10 +3,9 @@ package com.cp.springcloud.controller;
 import com.cp.springcloud.pojo.Dept;
 import com.cp.springcloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,6 +13,8 @@ import java.util.List;
 public class DeptController {
     @Autowired
     private DeptService deptService;
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @PostMapping("/dept/add")
     public boolean addDept(Dept dept){
@@ -29,5 +30,24 @@ public class DeptController {
     @GetMapping("/dept/list")
     public List<Dept> queryAll(){
         return deptService.queryAll();
+    }
+
+    @RequestMapping("/dept/discovery")
+    //注册进来的微服务 获取一些消息
+    public Object discovery(){
+        //获取微服务列表的清单
+        List<String> services = discoveryClient.getServices();
+        System.out.println("discovery - services:"+services);
+        //得到一个具体的微服务信息
+        List<ServiceInstance> instances = discoveryClient.getInstances("SPRINGCLOUD-PROVIDER-DEPT");
+        for (ServiceInstance instance : instances) {
+            System.out.println(
+                    instance.getHost()+"\t"+
+                    instance.getPort()+"\t"+
+                    instance.getUri()+"\t"+
+                    instance.getServiceId()+"\t"
+            );
+        }
+        return this.discoveryClient;
     }
 }
